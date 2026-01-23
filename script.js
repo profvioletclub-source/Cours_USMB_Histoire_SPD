7ae4f886dddac82be3bb417d59ef120dbdd5d9a1
leprof <prof.violet.club@gmail.com>
2026-01-23 12:14:01 +0100

    Add event listener for resources button

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn-ressources");
+
+    btn.addEventListener("click", () => {
+        alert("Les ressources seront bientôt disponibles !");
+    });
+});
